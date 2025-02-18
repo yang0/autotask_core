d5f4db51427ea7388233439b53df77d56d8df0d7
yang0 <yang0@126.com>
2025-02-19 06:43:49 +0800

    啊

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from .agents.simpleAgent import *
 # from .documentLoader.pdfLoader import *
 # from .documentLoader.sqliteLoader import *
 
-VERSION = "0.0.17"
+VERSION = "0.0.42"
 GIT_URL = "https://github.com/yang0/autotask_core.git"
 NAME = "AutoTask Core"
 DESCRIPTION = """Core plugin for AutoTask that provides essential AI agent features including:
