dad0080e9fd38416c3dfdafff9445da167e34152
yang0 <yang0@126.com>
2025-04-05 14:16:00 +0800

    'init'

--- __init__.py
+++ __init__.py
@@ -1,28 +1,11 @@
 from .nodes.basic import *
 from .nodes.iteratorNode import *
 from .nodes.assistant import *
-# from .nodes.duckduckgo import *
-# from .nodes.exa import *
-# from .nodes.python import *
-# from .nodes.baidu import *
-# from .nodes.webscraper import *
-# from .nodes.data_processor import *
-# from .nodes.yfinance import *
-# from .nodes.googlecalendar import *
-# from .nodes.youtube import *
-
 from .nodes.time import *
 from .embedder import *
 from .reader.text_reader import *
 from .assistants.default_assistant import *
-# from .nodes.browser_user import *
-# from .nodes.crawl4ai import *
-# from .nodes.x_free import *
-# from .nodes import *
-# from .documentLoader.jsonlLoader import *
-# from .documentLoader.largeTextLoader import *
-# from .documentLoader.pdfLoader import *
-# from .documentLoader.sqliteLoader import *
+
 
 
 VERSION = "0.0.44"
